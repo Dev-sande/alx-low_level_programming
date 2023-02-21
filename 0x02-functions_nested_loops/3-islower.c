2f78034d88e837d7fece5add55ab4c9a4585a14c
Dev-sande <tapiwasande9@gmail.com>
2023-02-21 10:30:08 -0800

    fixing code according to the Betty style

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
-
 /**
- * _islower - Checks for lowercase character
- * By Tapiwa Sande
- * Return: 1 for lowercase character or 0 for anything else
+ * _islower - Shows 1 if the input is a
+ * lowercase character. Another cases, shows
+ * 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase character. 0 for the rest.
  */
-
 int _islower(int c)
-
 {
-
 	if (c >= 97 && c <= 122)
 	{
-
-	return (1);
+		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * _abs - Computes the absolute value of an integer.
- * By Tapiwa Sande
- * Return: Absolute value of number or zero
+ * _abs - Computes the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
  */
-
-int _abs(int c)
-
+int _abs(int i)
 {
-
-	if (c < 0)
+	if (i >= 0)
 	{
-	int abs_val;
-
-	abs_val = c * -1;
-	return (abs_val);
+		return (i);
 	}
-	return (c);
+	else
+	{
+		return (i * -1);
+	}
 }
+
