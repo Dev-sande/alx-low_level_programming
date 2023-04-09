0f8b0563f3184883e664586d5e43e32a4d6afb12
Dev-sande <tapiwasande9@gmail.com>
2023-04-09 11:11:39 -0700

    initial commit commit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * A program that reads a text file and prints it to the POSIX standard output
+ * @letter: number of letter in the text file
+ * @filename: the text file we wish to read
+ */
+
+ ssize_t read_textfile(const char *filename, size_t letters)
+ {
+
+ char *buffer;
+ ssize_t taps, bytes_read, bytes_written;
+
+ taps = open(filename, O_RDONLY);
+ if (taps == -1)
+     return 0;
+
+buffer = malloc(sizeof(char) * letters);
+bytes_read = read(taps, buffer, letters);
+bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+free(buffer);
+close(taps);
+
+return bytes_written;
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+This folder contains files for the 0x15-file_io project from ALX
