b797d358a3e6f6ff9215d24e848374d53611cb99
Dev-sande <tapiwasande9@gmail.com>
2023-04-18 02:18:15 -0700

    _putchar.c errors fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,23 +2,26 @@
 
 void print_binary(unsigned long int input_number)
 {
-	int bit_position, set_bit_count = 0;
-	unsigned long int current_bit_value;
-	for (bit_position = 63; bit_position >= 0; bit_position--)
-	{
-		current_bit_value = input >> bit_position;
+    int bit_position, set_bit_count = 0;
+    unsigned long int current_bit_value;
+    for (bit_position = 63; bit_position >= 0; bit_position--)
+    {
+        current_bit_value = input_number >> bit_position;
 
-		if (current_bit_value = input_number >> bit_position;
-		{
-		if (current_bit_value & 1)
-                   _putchar('1');
-                   set_bit_count++;
-                }
+        if (current_bit_value & 1)
+        {
+            _putchar('1');
+            set_bit_count++;
+        }
+        else if (set_bit_count)
+        {
+            _putchar('0');
+        }
+    }
 
-                else if (set_bit_count)
-                    _putchar('0');		
+    if (!set_bit_count)
+    {
+        _putchar('0');
+    }
 }
 
-if (!set_bit_count)
-   _putchar('0');
-}
