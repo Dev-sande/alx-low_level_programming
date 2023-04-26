bdb401ea0f06cae452ed1a415977fa9ba5634879
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 10:49:45 -0700

    fixed syntanx errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,29 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * read_textfile - reads text file
- * @filename: filename.
- * @letters: letters to print out
- *
- * Return: return 1 on success and 0 on failure.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: name of file
+ * @letters: letters to printed
+ * Return: number of bytes
+ * 1 on success and 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
 	char *buf;
+	ssize_t pdf;
+	ssize_t n;
+	ssize_t t;
 
-	if (!filename)
+	pdf = open(filename, O_RDONLY);
+	if (pdf == -1)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
-		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
-	close(fd);
+	buf = malloc(sizeof(char) * letters);
+	t = read(pdf, buf, letters);
+	n = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-
-	return (nwr);
+	close(pdf);
+	return (n);
 }
+
