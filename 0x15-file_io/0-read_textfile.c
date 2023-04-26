f8f45d2833c1d3a451f9a264ea09b699a9d23126
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 10:34:12 -0700

    fixed syntax errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,29 +1,37 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-* A program that reads a text file and prints it to the POSIX standard output
-* @letters: number of letter in the text file
-* @filename: the text file we wish to read
-* @read_textfile: function name as required
-*/
-
+ * read_textfile - returns the number of
+ * bytes written to standard output
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: 0 is returned on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int pdf;
+	ssize_t hold, fac;
+	char *buf;
 
-char *buffer;
-ssize_t taps, bytes_read, bytes_written;
+	if (!filename)
+		return (0);
 
-taps = open(filename, O_RDONLY);
-  if (taps == -1)
-    return (0);
+	pdf = open(filename, O_RDONLY);
 
-buffer = malloc(sizeof(char) * letters);
-bytes_read = read(taps, buffer, letters);
-bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (pdf == -1)
+		return (0);
 
-free(buffer);
-close(taps);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-return (bytes_written);
+	hold = read(pdf, buf, letters);
+	fac = write(STDOUT_FILENO, buf, hold);
+
+	close(pdf);
+
+	free(buf);
+
+	return (fac);
 }
