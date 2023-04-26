66902ca015e2c9761f397200cc2e8e13515bb883
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 13:19:29 -0700

    initial commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,33 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
+#include "main.h"
 
 /**
-* @filename: is the name of the file we are creating
-* @text_context: is a NULL terminated string to write to the file
-*/
-
-int create_file(const char *filename, char *text)
+ * create_file - file creation.
+ * @filename: name of file.
+ * @text_content: pointer to a string.
+ *
+ * Return: 0 on success and 1 on failure
+ */
+int create_file(const char *filename, char *text_content)
 {
-	int taps, write_result, length = 0;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
-{
-           return (-1);
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
 
-
-if (text != NULL) {
-   length = strlen(text);
-}
-
-taps = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-if (taps == -1)
-{
-   return (-1);
-}
-
-write_result = write(taps, text, length);
-if (write_result == -1)
-{
-  close(taps);
-  return (-1);
-}
-
-close(taps);
-return (1);
-}
