2358556cb396cfcd59cee90a18d2eb5033dda371
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 04:56:13 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all our elements of a singly linked list of integers
+ * @k: head of a list.
+ * @pmd: local variable
+ * Return: numbers of nodes.
+ */
+size_t print_listint(const listint_t *k)
+{
+	size_t pmd = 0;
+
+	while (k != NULL)
+	{
+		printf("%d\n", k->n);
+		k = k->next;
+		pmd++;
+	}
+	return (pmd);
+}
+
