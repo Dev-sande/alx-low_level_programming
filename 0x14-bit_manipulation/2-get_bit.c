407f3e7336d3b81515fdb3a2f006ec5a01ef69de
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 07:20:41 -0700

    initial commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,17 +1,26 @@
 #include "main.h"
 
 /**
- * @index: is the index starting from 0
-*
-*/
-
-int get_bit(unsigned long int taps, unsigned int san)
-
+ * get_bit - returns the value of the bit at a given index in a byte.
+ * @n: byte to retrieve the bit from.
+ * @index: index of the bit.
+ *
+ * Return: value of the bit.
+ */
+int get_bit(unsigned long int n, unsigned int index)
 {
-	int dar;
-	if(san > 63)
-	return (-1);
+	unsigned int i;
 
-	dar =(taps >> san) & 1;
-	return (dar);
+	if (n == 0 && index < 64)
+		return (0);
+
+	for (i = 0; i <= 63; n >>= 1, i++)
+	{
+		if (index == i)
+		{
+			return (n & 1);
+		}
+	}
+
+	return (-1);
 }
