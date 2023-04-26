f8e549dca1b3ab2e52c3e9d381529ed214b4f088
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 06:45:52 -0700

    fixed betty style error

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,29 @@
 #include "main.h"
-
+/**
+* print_binary - prints out our binary
+*/
 void print_binary(unsigned long int input_number)
 {
-    int bit_position, set_bit_count = 0;
-    unsigned long int current_bit_value;
-    for (bit_position = 63; bit_position >= 0; bit_position--)
-    {
-        current_bit_value = input_number >> bit_position;
+int bit_position, set_bit_count = 0;
+unsigned long int current_bit_value;
+for (bit_position = 63; bit_position >= 0; bit_position--)
+{
+current_bit_value = input_number >> bit_position;
 
-        if (current_bit_value & 1)
-        {
-            _putchar('1');
-            set_bit_count++;
-        }
-        else if (set_bit_count)
-        {
-            _putchar('0');
-        }
-    }
+if (current_bit_value & 1)
+{
+_putchar('1');
+set_bit_count++;
+}
+else if (set_bit_count)
+{
+_putchar('0');
+}
+}
 
-    if (!set_bit_count)
-    {
-        _putchar('0');
-    }
+if (!set_bit_count)
+{
+_putchar('0');
+}
 }
 
