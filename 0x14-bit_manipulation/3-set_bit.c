4c81ff38bc92d7c6ca3231f8e926475b9f0fff35
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 09:58:56 -0700

    fixed trailing whitespaces

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,11 @@
 #include "main.h"
+/**
+ * set_bit - sets the value of a specific bit at the given index to 1.
+ * @n: pointer to unsigned long int.
+ * @index: index of the bit.
+ *
+ * Return: 1 if on pass and -1 on fail.
+ */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
@@ -6,4 +13,4 @@ int set_bit(unsigned long int *n, unsigned int index)
 		return (-1);
 	*n |= (1UL << index);
 	return (1);
-}	
+}
