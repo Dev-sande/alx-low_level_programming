02c40c7011602bd9a2c744e3fe18c26f3672df63
Dev-sande <tapiwasande9@gmail.com>
2023-04-26 10:21:19 -0700

    reduced excess parameters

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -4,8 +4,8 @@
 *
 * Returns 0 if the system is big-endian or 1 if it's little-endian.
 *
-* @num: stores the value 1 in an integer variable.
-* @t: pointer to determine the byte order of the system.
+* num: stores the value 1 in an integer variable.
+* t: pointer to determine the byte order of the system.
 *
 * Return: 0 if big-endian, 1 if little-endian
 */
