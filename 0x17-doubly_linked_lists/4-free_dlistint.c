0e53c1f8e74beb619a581d48931116bb1a153185
Dev-sande <tapiwasande9@gmail.com>
2023-05-18 23:55:11 +0000

    initial commit

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -3,20 +3,20 @@
 #include "lists.h"
 
 /**
- * free_dlistint - function that frees a dlistint_t list
- *
- * @head: header to list to be freed
- * Return: Nothing
- */
+* free_dlistint - function that frees a dlistint_t list
+*
+* @head: header to list to be freed
+* Return: Nothing
+*/
 void free_dlistint(dlistint_t *head)
 {
-        dlistint_t *temp;
+dlistint_t *temp;
 
-        while (head != NULL)
-        {
-                temp = head;
-                head = head->next;
-                free(temp);
-        }
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
 }
 
