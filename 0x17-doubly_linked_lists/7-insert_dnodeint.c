a06d4a822f25e4c4ff6cff25e66408be7621d7a4
Dev-sande <tapiwasande9@gmail.com>
2023-05-20 16:30:27 +0000

    fixed syntax error in 7-insert_dnodeint.c file

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,82 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * createNode - Creates a new node for the doubly linked list
- * @n: The value to be stored in the new node
- *
- * Return: Pointer to the newly created node
- */
-dlistint_t *createNode(int n)
-{
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-
-	if (newNode == NULL)
-	{
-		printf("Memory allocation failed.\n");
-		exit(EXIT_FAILURE);
-	}
-
-	newNode->n = n;
-	newNode->prev = NULL;
-	newNode->next = NULL;
-	return (newNode);
-}
-
-/**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a
- * doubly linked list
- * @h: Address of the head of the doubly linked list
- * @idx: Index at which the new node should be inserted
- * @n: Value to be stored in the new node
- *
- * Return: Pointer to the newly inserted node, or NULL on failure
- */
+* insert_dnodeint_at_index - Inserts a new node at a given position.
+* @h: A pointer to the head of the doubly linked list.
+* @idx: The index where the new node should be added.
+* @n: The data value of the new node.
+*
+* Return: The address of the new node, or NULL if it failed.
+*/
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	if (h == NULL)
-		return (NULL);
+dlistint_t *new_node, *current_node;
+unsigned int i;
 
-	dlistint_t *current = *h;
-	unsigned int i = 0;
-	dlistint_t *newNode = createNode(n);
+if (h == NULL)
+return (NULL);
 
-	if (newNode == NULL)
-		return (NULL);
+/* Create a new node */
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
 
-	if (idx == 0)
-	{
-		newNode->next = *h;
-		if (*h != NULL)
-			(*h)->prev = newNode;
-		*h = newNode;
-		return (newNode);
-	}
-
-	while (i < idx - 1)
-	{
-		if (current == NULL)
-		{
-			free(newNode);
-			return (NULL);
-		}
-		current = current->next;
-		i++;
-	}
-
-	if (current == NULL)
-	{
-		free(newNode);
-		return (NULL);
-	}
-
-	newNode->next = current->next;
-	newNode->prev = current;
-	if (current->next != NULL)
-		current->next->prev = newNode;
-	current->next = newNode;
-
-	return (newNode);
+/* Insert at the beginning */
+if (idx == 0)
+{
+new_node->prev = NULL;
+new_node->next = *h;
+if (*h != NULL)
+(*h)->prev = new_node;
+*h = new_node;
+return (new_node);
 }
 
+/* Traverse to the desired position */
+current_node = *h;
+for (i = 0; i < idx - 1 && current_node != NULL; i++)
+current_node = current_node->next;
+
+/* If index is out of bounds */
+if (current_node == NULL)
+{
+free(new_node);
+return (NULL);
+}
+
+/* Insert at the end */
+if (current_node->next == NULL)
+{
+new_node->prev = current_node;
+new_node->next = NULL;
+current_node->next = new_node;
+return (new_node);
+}
+
+/* Insert in the middle */
+new_node->prev = current_node;
+new_node->next = current_node->next;
+current_node->next->prev = new_node;
+current_node->next = new_node;
+
+return (new_node);
+}
